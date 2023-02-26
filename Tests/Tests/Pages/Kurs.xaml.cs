3b12ce91e2d862d66998a2f3f43a4f94ad03a43c
kublahan <67871033+kublahan@users.noreply.github.com>
2023-02-26 12:27:29 +0300

    Initial commit

---
+++ Tests/Tests/Pages/Kurs.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tests.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Kurs.xaml
+    /// </summary>
+    public partial class Kurs : Page
+    {
+        public Kurs()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Test());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new KursChoise());
+        }
+    }
+}
